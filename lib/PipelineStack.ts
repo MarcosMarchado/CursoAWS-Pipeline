a37be0c4f260b5776b5892ac1df8f8d847abd148
Marcos Machado <marcoseu1994@gmail.com>
2022-08-19 23:48:48 -0300

    Pipeline funcional

--- lib/PipelineStack.ts
+++ lib/PipelineStack.ts
@@ -1,13 +1,19 @@
-import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnCapabilities, RemovalPolicy, SecretValue, Stack, StackProps } from 'aws-cdk-lib';
 import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
 import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
-import { CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { CloudFormationCreateReplaceChangeSetAction, CloudFormationExecuteChangeSetAction, CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { Effect, ManagedPolicy, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import * as Yaml from "yaml"
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const artifactsBucket = new Bucket(this, "S3BucketForPipelineArtifacts", {
+      removalPolicy: RemovalPolicy.DESTROY
+    });
 
     const pipeline = new Pipeline(this, "Pipeline", {
       pipelineName: "Pipeline",
@@ -23,34 +29,75 @@ export class PipelineStack extends Stack {
           owner: "MarcosMarchado",
           repo: "lambda-quarkus",
           branch: "main",
-          actionName: "Pipeline_Source",
+          actionName: "Source",
           oauthToken: SecretValue.secretsManager("github-token"),
           output: sourceOutput
         })
       ]
     })
 
-    const cdkBuildOutput = new Artifact("CdkBuildOutput")
+    const buildArtifact = new Artifact("BuildArtifact")
+
+    const pipelineProject = new PipelineProject(this, "CodeBuildProject", {
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_5_0,
+        privileged: true
+      },
+      buildSpec: BuildSpec.fromObjectToYaml(this.getBuildSpecYml(artifactsBucket.bucketName))
+    })
+
+    pipelineProject.role?.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"))
+    //artifactsBucket.grantReadWrite(pipeline.role) Tentar com essa alternativa
 
     pipeline.addStage({
       stageName: "Build",
       actions: [new CodeBuildAction({
-        actionName: "CDK_Build",
+        actionName: "BuildAction",
         input: sourceOutput,
-        outputs: [cdkBuildOutput],
-        project: new PipelineProject(this, "CdkBuildProject", {
-          environment: {
-            buildImage: LinuxBuildImage.STANDARD_5_0,
-            privileged: true
-          },
-          buildSpec: BuildSpec.fromObjectToYaml(this.getBuildSpecYml())
-        })
+        outputs: [buildArtifact],
+        project: pipelineProject
       })]
     })
 
+    //Deploy Stage
+    const stackName = 'Codepipeline-Lambda-Stack';
+    const changeSetName = 'StagedChangeSet'
+
+    const createReplaceChangeSetAction = new CloudFormationCreateReplaceChangeSetAction({
+      actionName: "PrepareChanges",
+      stackName: stackName,
+      changeSetName: changeSetName,
+      templatePath: buildArtifact.atPath('outputtemplate.yml'),
+      cfnCapabilities: [
+        CfnCapabilities.NAMED_IAM,
+        CfnCapabilities.AUTO_EXPAND
+      ],
+      adminPermissions: false,
+      runOrder: 1
+    });
+
+    const executeChangeSetAction = new CloudFormationExecuteChangeSetAction({
+      actionName: "ExecuteChanges",
+      changeSetName: changeSetName,
+      stackName: stackName,
+      runOrder: 2
+    })
+
+    pipeline.addStage({
+      stageName: "Deploy",
+      actions: [
+        createReplaceChangeSetAction,
+        executeChangeSetAction
+      ],
+    });
+
+    //Permission for CloudFormation to access Lambda and other resources
+    createReplaceChangeSetAction.deploymentRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AWSLambdaExecute'));
+    createReplaceChangeSetAction.deploymentRole.attachInlinePolicy(this.getCodePipelineCloudFormationInlinePolicy());
   }
+  //https://docs.aws.amazon.com/pt_br/serverless-application-model/latest/developerguide/sam-cli-command-reference-sam-package.html
   //https://stackoverflow.com/questions/71406505/how-to-writing-the-buildspec-yaml-code-in-cdk
-  getBuildSpecYml() {
+  getBuildSpecYml(bucket: string) {
     return Yaml.parse(`
     version: 0.2
 
@@ -66,9 +113,42 @@ export class PipelineStack extends Stack {
       build:
         commands:
           - mvn clean package
+          - export BUCKET=${bucket}
+          - sam package --s3-bucket $BUCKET --template target/sam.jvm.yaml --output-template-file outputtemplate.yml
     
     artifacts:
+      types: zip
       files:
-        - target/function.zip`)
+        - outputtemplate.yml
+        - target/sam.jvm.yaml`)
+  }
+
+  //Inline permission policy for CloudFormation
+  private getCodePipelineCloudFormationInlinePolicy = () => {
+    return new Policy(this, 'CodePipelineCloudFormationInlinePolicy', {
+      statements: [
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: [
+            "apigateway:*",
+            "codedeploy:*",
+            "lambda:*",
+            "cloudformation:CreateChangeSet",
+            "iam:GetRole",
+            "iam:CreateRole",
+            "iam:DeleteRole",
+            "iam:PutRolePolicy",
+            "iam:AttachRolePolicy",
+            "iam:DeleteRolePolicy",
+            "iam:DetachRolePolicy",
+            "iam:PassRole",
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "s3:GetBucketVersioning"
+          ],
+          resources: ['*']
+        })
+      ]
+    })
   }
 }
